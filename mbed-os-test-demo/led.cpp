f755be84756d9493ca0713ab67bdde7c14cc7d0a
ee2405 <dean03318Wgmail.com>
2020-03-11 18:06:12 +0800

    45

---
+++ mbed-os-test-demo/led.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+void Led3(DigitalOut& ledpin)
+{
+   for(int i=0; i<6; ++i) { //blink for 10 times
+       ledpin = !ledpin; // toggle led
+       wait(0.2f);
+   }
+}
+void Led2(DigitalOut& ledpin)
+{
+   for(int i=0; i<4; ++i) { //blink for 10 times
+       ledpin = !ledpin; // toggle led
+       wait(0.2f);
+   }
+}
